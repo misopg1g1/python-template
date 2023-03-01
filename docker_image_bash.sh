433c4a9918b96ff6db3c517a14659fc145e1bb0c
EdgarLuna <eluna@urchinsys.com>
2023-03-01 13:06:13 -0500

    new bash para generar y subir imagen de docker

--- docker_image_bash.sh
+++ docker_image_bash.sh
@@ -1,3 +1,3 @@
-docker build . --file Dockerfile --tag ${REGISTRY_HOST}/${REGISTRY_NAME}/${REPO_NAME}-${BRANCH_NAME}:${image_version} --tag ${REGISTRY_HOST}/${REGISTRY_NAME}/${REPO_NAME}-${BRANCH_NAME}:latest
-docker push ${REGISTRY_HOST}/${REGISTRY_NAME}/${REPO_NAME}-${BRANCH_NAME}:${IMAGE_VERSION}
-docker push ${REGISTRY_HOST}/${REGISTRY_NAME}/${REPO_NAME}-${BRANCH_NAME}:latest
+docker build . --file Dockerfile --tag ${REGISTRY_HOST}/${REGISTRY_SPACE}/${REPO_NAME}-${BRANCH_NAME}:${image_version} --tag ${REGISTRY_HOST}/${REGISTRY_SPACE}/${REPO_NAME}-${BRANCH_NAME}:latest
+docker push ${REGISTRY_HOST}/${REGISTRY_SPACE}/${REPO_NAME}-${BRANCH_NAME}:${IMAGE_VERSION}
+docker push ${REGISTRY_HOST}/${REGISTRY_SPACE}/${REPO_NAME}-${BRANCH_NAME}:latest
